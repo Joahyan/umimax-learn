7a037df77fd350157d01b19fa66f21153513c859
joahyan <joahyan@163.com>
2023-02-16 12:01:44 +0800

    feat: 菜单栏样式及展开收缩功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: require.resolve('@umijs/max/eslint')
+  extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false
+        }
+      }
+    ],
+    camelcase: [0, { ignoreDestructuring: true, properties: 'never' }]
+  }
 };
